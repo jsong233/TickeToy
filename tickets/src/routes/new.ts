e6e215705f94d09be74418e68842227848e0a44d
Joy Song <jsong97224@gmail.com>
2023-04-04 16:51:19 -0600

    introduce NATS

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@jsticketoy/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -24,6 +26,13 @@ async (req: Request, res: Response) => {
   });
 
   await ticket.save();
+  // publish the event to NATS
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  });
 
   res.status(201).send(ticket);
 });
